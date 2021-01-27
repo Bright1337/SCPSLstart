dad0ad2d5684c784e84b10e9c46b1b18098998dd
Bright1337 <55995111+Bright1337@users.noreply.github.com>
2021-01-27 08:15:29 +0100

    Update exiled.sh

--- exiled.sh
+++ exiled.sh
@@ -2,6 +2,8 @@
 steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update 996560 +quit
 NewVer=$(curl https://api.github.com/repos/galaxy119/EXILED/tags?per_page=1 | grep -n "name"|cut -c2-| grep -o '[0-9].[0-9].[0-9][0-9]')
 NewVer="2.1.28"
+mono = "bab758d1eb35ccaf68bf599a471fe58379a3e627  Mono.Posix.dll"
+multi = "fd51a13dcc129d720ba23f9c8f693b25d4f5c9eb  MultiAdmin.exe"
 ThisVerFile=cur.ver
 ThisVer=$(cat $ThisVerFile)
 if test -f "$ThisVerFile"; then
@@ -55,6 +57,16 @@ else
         rm -r exiled/
         echo "$NewVer" > $ThisVerFile
 fi
+if shasum Mono.Posix.dll == mono; then
+        echo "Gucci"
+else
+        curl -L -O https://github.com/Bright1337/SCPSLstart/releases/download/1.1.1/Mono.Posix.dll
+fi
+if shasum MultiAdmin.exe == multi; then
+        echo "Gucci"
+else
+        curl -L -O https://github.com/Bright1337/SCPSLstart/releases/download/1.1.1/MultiAdmin.exe
+fi
 cd
-$1
+mono MultiAdmin.exe --port $1
 
