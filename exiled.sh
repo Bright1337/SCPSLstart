32aab8674e1d3088f924e49798869ff1bd902dd1
Bright1337 <55995111+Bright1337@users.noreply.github.com>
2021-02-19 08:21:43 +0100

    Update exiled.sh

--- exiled.sh
+++ exiled.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update 996560 +quit
 NewVer=$(curl https://api.github.com/repositories/231269519/tags?per_page=1 | grep -n "name"|cut -c2-| grep -o '[0-9].[0-9].[0-9]')
-
+NewVer="2.2.4"
 mono = "bab758d1eb35ccaf68bf599a471fe58379a3e627  Mono.Posix.dll"
 multi = "fd51a13dcc129d720ba23f9c8f693b25d4f5c9eb  MultiAdmin.exe"
 ThisVerFile=cur.ver
